c7416de4b1ae0b773cd1f871aa2f94f56c2074f2
Junglepanda69 <Conrady82@gmail.com>
2024-01-30 16:19:49 -0800

    backend fixed

---
+++ backend/lambda_database_ops/handler.js
@@ -0,0 +1,7 @@
+exports.handler = async (event, context) => {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, I am lambda_database_ops" }),
+  };
+};
